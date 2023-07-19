4c0b5db448d3bae84eed4c7fe7c306181cf55115
Niranjan Biswas <yuvrajbiswas1435.yb718@gmail.com>
2023-07-19 18:26:16 +0530

    url exchange

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
-const port = process.env.PORT || 7777;
+const port = process.env.PORT
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io")
  
-          
+  //  https://livevideoapp.onrender.com       
 const io = new Server(server, {
   cors: {
-    origin: "https://livevideoapp.onrender.com",
+    origin:
+      "https://64b7cd75dc48e600978ac4ed--niranjanlivevideoapp.netlify.app/",
     methods: ["GET", "POST"],
   },
 }); 
